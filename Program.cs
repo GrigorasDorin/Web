b1224f2b719bb80587d2b232129e77194650d962
GrigorasDorin <dorinngrigoras@gmail.com>
2020-08-20 17:20:37 +0300

    Cleaned Program.cs file

--- Program.cs
+++ Program.cs
@@ -4,24 +4,9 @@ namespace Web
 {
     class Program
     {
-        class ZipExtension : IFileCompression, IComparable
-        {
-            int IComparable.CompareTo(object obj)
-            {
-                throw new NotImplementedException();
-            }
-
-            public void Compress(string targetFileName, string[] fileList) { }
-            public void Uncompress(string compressedFileName, string expanddirectoryName) { }
-        }
-
         static void Main(string[] args)
         {
-            ZipExtension ext = new ZipExtension();
-            ext.Compress("", new string[] { "", "" });
 
-            // Cast the ZipExtension object otherwise a compile error pops-up
-            ((IComparable)ext).CompareTo(new ZipExtension());
         }
     }
 }
